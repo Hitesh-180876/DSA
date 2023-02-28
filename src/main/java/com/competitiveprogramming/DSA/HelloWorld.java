c99ce5f7b167db0e66209565fcb9073e5e9daf11
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2023-02-28 23:16:17 +0530

    issue resolved

--- src/main/java/com/competitiveprogramming/DSA/HelloWorld.java
+++ src/main/java/com/competitiveprogramming/DSA/HelloWorld.java
@@ -3,7 +3,5 @@ package com.competitiveprogramming.DSA;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
     }
 }
